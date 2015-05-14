98f721ec5f4354ca1c098d34a17e81904800c020
Lymar Petro <Lymarpiter@gmail.com>
2015-05-14 20:20:01 +0300

    mamapapa output

--- IDPHomeWork Lesson 1/MamaPapaOutput.c
+++ IDPHomeWork Lesson 1/MamaPapaOutput.c
@@ -7,3 +7,27 @@
 //
 
 #include "MamaPapaOutput.h"
+
+
+
+extern
+int MCLMamaPapaOutput(int value) {
+    if (value % 15 == 0) {
+        printf("mamapapa\n");
+        return 15;
+    }
+    
+    if (value % 3 == 0) {
+        printf("mama\n");
+        return 3;
+    }
+    
+    if (value % 5 == 0) {
+        printf("papa\n");
+        return 5;
+    }
+    
+    return 0;
+}
+
+
