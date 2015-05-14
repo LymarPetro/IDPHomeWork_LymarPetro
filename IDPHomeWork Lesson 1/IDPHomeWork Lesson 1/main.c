a34f94aef2e74170649ff40076931e482f169a87
Lymar Petro <Lymarpiter@gmail.com>
2015-05-14 20:29:07 +0300

    mamapapa output done

--- IDPHomeWork Lesson 1/IDPHomeWork Lesson 1/main.c
+++ IDPHomeWork Lesson 1/IDPHomeWork Lesson 1/main.c
@@ -8,15 +8,23 @@
 
 #include <stdio.h>
 
+#include "MamaPapaOutput.h"
+
 int main(int argc, const char * argv[]) {
-    printf("void size output %lu\n", sizeof(void));
-    printf("char size output %lu\n", sizeof(char));
-    printf("short int size output = %lu\n", sizeof(short int));
-    printf("float size output %lu\n", sizeof(float));
-    printf("int size output =  %lu\n",  sizeof(int));
-    printf("long int size output = %lu\n", sizeof(long int));
-    printf("double size output %lu\n", sizeof(double));
+//    printf("void size output %lu\n", sizeof(void));
+//    printf("char size output %lu\n", sizeof(char));
+//    printf("short int size output = %lu\n", sizeof(short int));
+//    printf("float size output %lu\n", sizeof(float));
+//    printf("int size output =  %lu\n",  sizeof(int));
+//    printf("long int size output = %lu\n", sizeof(long int));
+//    printf("double size output %lu\n", sizeof(double));
     
+   
+    int value;
+    
+    scanf("%d", &value);
+    
+    MCLMamaPapaOutput(value);
     
     return 0;
 }
