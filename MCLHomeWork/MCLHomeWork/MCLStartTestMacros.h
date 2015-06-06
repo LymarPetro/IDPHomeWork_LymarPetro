4b7b99cee8205a6c1e243403d3e3abf2725c900c
Lymar Petro <Lymarpiter@gmail.com>
2015-06-06 18:44:11 +0300

    push test

---
+++ MCLHomeWork/MCLHomeWork/MCLStartTestMacros.h
@@ -0,0 +1,14 @@
+//
+//  MCLTestMacros.h
+//  MCLHomeWork
+//
+//  Created by Велес Тяжеловоз on 06.06.15.
+//  Copyright (c) 2015 MechanicalСrabLab. All rights reserved.
+//
+
+#ifndef __MCLHomeWork__MCLTestMacros__
+#define __MCLHomeWork__MCLTestMacros__
+
+#include <stdio.h>
+
+#endif /* defined(__MCLHomeWork__MCLTestMacros__) */
